1001ab9005457e0b54811eabc426c2667d0db17c
Ricardo Gimenez <rmgimenez@gmail.com>
2023-03-31 23:47:11 -0300

    resolvido problema 9

--- 0009.js
+++ 0009.js
@@ -7,3 +7,20 @@ For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 */
+
+// trinagulo 3, 4 e 5
+
+limite = 1000;
+
+function encontraHipotenusa(a, b) {
+  return Math.sqrt(a * a + b * b);
+}
+
+for (let i = 3; i < limite - 3; i++) {
+  for (let j = i + 1; j < limite - 2; j++) {
+    let c = encontraHipotenusa(i, j);
+    if (i + j + c == 1000) {
+      console.log(i * j * c);
+    }
+  }
+}
