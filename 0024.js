b338d0aff02c36bffb8432babbcfeb1b2d2e459f
Ricardo Gimenez <cpd2@santanna.g12.br>
2023-11-21 16:13:36 -0300

    problema 24 finalizado

--- 0024.js
+++ 0024.js
@@ -12,4 +12,29 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 
 */
 
-function geradorPermutacao(num) {}
+function nthLexicographicPermutation(elements, n) {
+  let result = [];
+  n--; // Ajuste para indexação base 0
+
+  while (elements.length > 0) {
+    let index = Math.floor(n / factorial(elements.length - 1));
+    n %= factorial(elements.length - 1);
+    result.push(elements.splice(index, 1)[0]);
+  }
+
+  return result;
+}
+
+// Função para calcular o fatorial
+function factorial(num) {
+  if (num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+let elements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let n = 1000000;
+let result = nthLexicographicPermutation(elements.slice(), n); // Copia da lista para preservar a original
+
+console.log(result.join(''));
