85219007f615950b86e87a423fa596d5e50c6d5c
Ricardo Gimenez <cpd2@santanna.g12.br>
2024-01-10 10:06:22 -0300

    terminei o problema 28

--- 0028.js
+++ 0028.js
@@ -1,9 +1,20 @@
-const numerosParaSomar = [];
+let diagonais = [];
 
-let contador = 0;
-
-for (let i = 1; i <= 25; i++) {
-  if (i == 1) {
-    numerosParaSomar.push(i);
+function retornaNumerosDasDiagonais(tamanhoLado) {
+  if (tamanhoLado == 1) {
+    diagonais.push(1);
+    return;
   }
+  let primeiraDiagonal = tamanhoLado ** 2;
+
+  diagonais.push(primeiraDiagonal);
+  diagonais.push(primeiraDiagonal - tamanhoLado + 1);
+  diagonais.push(primeiraDiagonal - tamanhoLado * 2 + 2);
+  diagonais.push(primeiraDiagonal - tamanhoLado * 3 + 3);
 }
+
+for (let i = 1; i <= 1001; i = i + 2) {
+  retornaNumerosDasDiagonais(i);
+}
+
+console.log(diagonais.reduce((partialSum, a) => partialSum + a, 0));
